3a0023928824547406bb6463ea767a69d0d97ead
Kevin McGill <kevmcgill@gmail.com>
2023-12-16 22:11:21 +1300

    Create a keymaps config file

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -3,4 +3,4 @@ vim.cmd("set tabstop=2") -- insert 2 spaces for a tab
 vim.cmd("set shiftwidth=2") -- the number of spaces inserted for each indentation
 vim.cmd("set softtabstop=2") -- insert 2 spaced for a tab, when editing
 vim.cmd("set clipboard=unnamedplus") -- allows neovim to access the system clipboard
-vim.g.mapleader = " "
+
