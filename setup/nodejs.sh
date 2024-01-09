2c1ecfea4f2f7e1119394bd74f814ab991c80898
Kevin McGill <kevmcgill@gmail.com>
2024-01-09 18:12:15 +1300

    Update nodejs.sh

--- setup/nodejs.sh
+++ setup/nodejs.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 asdf plugin add nodejs
-asdf install nodejs 16.20.2
-asdf install nodejs 18.19.0
-asdf install nodejs 20.10.0
-asdf global nodejs 18.19.0
+asdf install nodejs 18.12.0
+asdf global nodejs 18.12.0
