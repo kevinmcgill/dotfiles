9e05eb816dbe02cb1d63c227dee05e54b976536b
Kevin McGill <kevmcgill@gmail.com>
2024-06-23 23:10:19 +1200

    Resolve gemfile path
    Closes https://github.com/kevinmcgill/dotfiles/issues/6

--- setup/ruby.sh
+++ setup/ruby.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+# Get the directory of the current script
+SCRIPT_DIR=$(dirname "$0")
+
+# Determine the absolute path to the Gemfile
+GEMFILE_PATH=$(cd "$SCRIPT_DIR/../ruby" && pwd)/Gemfile
+
 asdf plugin add ruby
 asdf install ruby 3.3.1
 asdf global ruby 3.3.1
 
-bundle install --gemfile=./ruby/Gemfile
+# Install gems using the absolute path to the Gemfile
+bundle install --gemfile="$GEMFILE_PATH"
