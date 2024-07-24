9c5e78703156eb600203c8bd828d50cc39f7b1da
Kevin McGill <kevmcgill@gmail.com>
2024-07-24 17:13:24 +1200

    Allow telescope to include hidden files (eg `.tool-versions`) and directories BUT exclude `.git/`
    This appends additional flags to the default `vimgrep_arguments` which
    is used when searching for words.
    Also we need to manually override the find files command to do the same
    thing (it does not use `vimgrep_arguments`.
    
    See
    https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#file-and-text-search-in-hidden-files-and-directories

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -11,6 +11,42 @@ return {
       vim.keymap.set('n', '<leader>q', builtin.quickfix, {})
       vim.keymap.set('n', '<leader>r', builtin.resume, {})
       vim.keymap.set('n', '<leader>w', builtin.grep_string, {})
+
+      local telescope_config = require("telescope.config")
+
+      -- Clone the default Telescope configuration
+      table.unpack = table.unpack or unpack -- 5.1 compatibility, nvim uses an older version of Lua internally
+      local vimgrep_arguments = { table.unpack(telescope_config.values.vimgrep_arguments) }
+
+      -- I want to search in hidden/dot files.
+      table.insert(vimgrep_arguments, "--hidden")
+      -- I don't want to search in the `.git` directory.
+      table.insert(vimgrep_arguments, "--glob")
+      table.insert(vimgrep_arguments, "!**/.git/*")
+      local setup = require('telescope').setup
+      setup {
+        defaults = {
+          -- `hidden = true` is not supported in text grep commands.
+          vimgrep_arguments = vimgrep_arguments,
+        },
+        pickers = {
+          buffers = {
+            sort_lastused = true,
+            mappings = {
+              i = {
+                ["<c-d>"] = require('telescope.actions').delete_buffer,
+              },
+              n = {
+                ["<c-d>"] = require('telescope.actions').delete_buffer,
+              },
+            },
+          },
+          find_files = {
+            -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+            find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+          },
+        },
+      }
     end
   },
   {
