b93d4b364cebaeea96a728d19be46fff5234372e
Kevin McGill <kevmcgill@gmail.com>
2024-07-24 17:06:00 +1200

    Tool version cleanup and simplify gem installation of tmuxinator and bundler
    Removing ruby, elixir and erlang as version we care about. Realistically
    I want all tools to be installed globally and having different tool
    versions (this project vs globally) was causing issues.
    Sometimes the local ruby version in the project `.tool-versions` file
    was out of sync with the global ruby version and this was causing
    tmuxinator to be unavailable globally.

--- setup/ruby.sh
+++ setup/ruby.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Get the directory of the current script
-SCRIPT_DIR=$(dirname "$0")
-
-# Determine the absolute path to the Gemfile
-GEMFILE_PATH=$(cd "$SCRIPT_DIR/../ruby" && pwd)/Gemfile
-
 # Install dependencies for building Ruby
 brew install openssl readline libyaml gmp
 
@@ -16,5 +10,5 @@ asdf global ruby 3.3.4
 # Re-source ASDF to ensure the installed Ruby version is available
 . "$HOME/.asdf/asdf.sh"
 
-# Install gems using the absolute path to the Gemfile
-bundle install --gemfile="$GEMFILE_PATH"
+gem install bundler
+gem install tmuxinator
