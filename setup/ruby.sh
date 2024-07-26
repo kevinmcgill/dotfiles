985fc0750f16ee4ed6c315e4fda5521507b83e2c
Kevin McGill <kevmcgill@gmail.com>
2024-07-26 12:09:08 +1200

    ruby-lsp and solargraph are installed globally manually to avoid Mason issues with different ruby project versions
    This was an ongoing problem for me, ruby-lsp and solargraph would have
    problems when moving from one project to another or one ruby version to
    another. Hopefully moving the installation (and management) to a `gem
    install` will simplify things. This impacts how these LSPs are
    configured in Neovim.

--- setup/ruby.sh
+++ setup/ruby.sh
@@ -12,3 +12,8 @@ asdf global ruby 3.3.4
 
 gem install bundler
 gem install tmuxinator
+
+# Install Ruby Language Server tools that we want globally available
+# Standardrb and Rubocop are installed in the project's Gemfile
+gem install ruby-lsp
+gem install solargraph
