02999ca90618044f84f6daf320977dcd7474b5bd
Kevin McGill <kevmcgill@gmail.com>
2024-07-28 00:20:41 +1200

    Add a tokyo night storm theme to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,7 +13,7 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Dracula (Official)"
+config.color_scheme = "Tokyo Night Storm"
 config.font = wezterm.font("BlexMono Nerd Font Mono")
 config.font_size = 14
 
