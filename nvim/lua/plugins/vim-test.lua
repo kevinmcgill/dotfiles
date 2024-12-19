79323ebd24299304b33db2bda13f68c604e39839
Kevin McGill <kevmcgill@gmail.com>
2024-12-19 23:21:59 +1300

    feat(nvim): vim test now runs tests in the neovim terminal, not a tmux split
    This was a side effect of removing the nvim-tmux navigation plugin

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -1,11 +1,15 @@
 return {
   "vim-test/vim-test",
-  dependencies = {
-    "preservim/vimux",
-  },
+  -- dependencies = {
+  --   "preservim/vimux",
+  -- },
   vim.keymap.set("n", "<leader>t", ":TestNearest<CR>"),
   vim.keymap.set("n", "<leader>tf", ":TestFile<CR>"),
   vim.keymap.set("n", "<leader>tl", ":TestLast<CR>"),
   vim.keymap.set("n", "<leader>gt", ":TestVisit<CR>"),
-  vim.cmd("let test#strategy = 'vimux'"),
+  vim.cmd("let test#strategy = 'neovim_sticky'"),
+  -- open the terminal in normal mode, so it can be interacted with.
+  vim.cmd("let g:test#neovim#start_normal = 1"),
+  -- let test#neovim#term_position = "vert botright 20"
+  vim.cmd("let test#neovim#term_position = 'horizontal bo 20'"),
 }
