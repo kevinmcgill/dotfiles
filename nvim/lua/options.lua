e2e33665fe424b18a543954b94b523db2ad8fc22
Kevin McGill <kevmcgill@gmail.com>
2025-05-07 22:37:08 +1200

    chore(nvim): simplify how closed folds appear
    By default foldtext shows a filtered version of the first line, witch
    some characters removed.
    When set to an empty string, foldtext is disabled, and the line is
    displayed normally with highlighting and no line wrapping.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -26,4 +26,5 @@ vim.cmd([[set iskeyword+=-]])
 vim.cmd("set foldmethod=expr") -- treesitter folding, see https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#folding
 vim.cmd("set foldexpr=nvim_treesitter#foldexpr()") -- treesitter folding
 vim.cmd("set nofoldenable") -- Disable folding at startup
+vim.cmd("set foldtext=''") -- Disable foldtext
 vim.cmd("set diffopt+=vertical") -- start diff mode in vertical split. See `:help diffopt`. Use :diffsplit and :diffthis to compare files
