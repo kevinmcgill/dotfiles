10599e0fc11a56839ff9f177dcc2c1d7220e069c
Kevin McGill <kevmcgill@gmail.com>
2025-05-07 22:40:36 +1200

    fix(nvim): neovim 0.11 update broke existing diagnostics config

--- nvim/lua/diagnostics.lua
+++ nvim/lua/diagnostics.lua
@@ -1,22 +1,27 @@
-local signs = {
-  { name = "DiagnosticSignError", text = "" },
-  { name = "DiagnosticSignWarn", text = "" },
-  { name = "DiagnosticSignInfo", text = "" },
-  { name = "DiagnosticSignHint", text = "" },
-}
-
-for _, sign in ipairs(signs) do
-  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-end
-
 local config = {
   virtual_text = {
     severity = {
       min = vim.diagnostic.severity.WARN,
     },
+    current_line = false, -- show virtual text diagnotistics except on current line
   },
+  virtual_lines = {
+    current_line = true, -- show virtual lines diagnotostics only on current line
+  },
+  severity_sort = true,
   signs = {
-    active = signs,
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.INFO] = "",
+      [vim.diagnostic.severity.HINT] = "",
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = 'hl-DiagnosticError',
+      [vim.diagnostic.severity.WARN] = 'hl-DiagnosticWarn',
+      [vim.diagnostic.severity.INFO] = 'hl-DiagnosticInfo',
+      [vim.diagnostic.severity.HINT] = 'hl-DiagnosticHint',
+    },
   },
   underline = {
     severity = {
