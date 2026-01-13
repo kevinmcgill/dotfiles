b19d1ac823588a982e87712395573560c30328b3
Kevin McGill <kevmcgill@gmail.com>
2026-01-13 16:34:14 +1300

    fix(nvim): bump node to allow copilot to use node:sqlite
    Copilot requires node:sqlite (which is an experimental feature of node).

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,5 +1,11 @@
 -- Description: Copilot plugin configuration
 
 return {
-  "github/copilot.vim",
+  'github/copilot.vim',
+  init = function()
+    local cur = vim.env.NODE_OPTIONS or ''
+    if not cur:match('%-%-experimental%-sqlite') then
+      vim.env.NODE_OPTIONS = (cur ~= '' and (cur .. ' ') or '') .. '--experimental-sqlite'
+    end
+  end,
 }
